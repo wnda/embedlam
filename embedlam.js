177e9c9f5040fe75017a063cd014bf475805ed4e
A. M. Douglas <me@amdouglas.com>
2016-12-21 04:57:11 +0000

    Update embedlam.js

--- embedlam.js
+++ embedlam.js
@@ -17,27 +17,41 @@
   win.addEventListener('scroll', debounce(checkDocument, 500), false);
 
   function checkDocument () {
+    // var is function scoped; initialise a variable outside of the for loop
+    // this way, we don't need to rely on [].forEach's closure, and we can
+    // use the much faster standard for loop.
     var iframe_src = '';
-    for (var i = 0; i < anchors.length; ++i) {
+    var i = 0;
+
+    for (; i < anchors.length; ++i) {
 
       if (typeof anchors[i].href === 'undefined' || anchors[i].href.length < 5 || !isInViewport(anchors[i])) {
+        // skip links that have no href, are hashlinks, or are not visible
+        // we could later expose an API to only select anchors with a data-attribute like 'data-embed-me=true'
         continue;
       }
 
+      // send the anchor's href contents to the embed URL encoder
       iframe_src = processUrl(anchors[i]);
 
       if (typeof iframe_src === 'string' && iframe_src.length > 0) {
+        // send the processed embed URL to the iframe factory
         makeInlineFrame(iframe_src, anchors[i], true);
+
+        // reset variable
         iframe_src = '';
       }
     }
   }
 
   function processUrl (_link) {
-    var _url = _link.href;
-    var _by_dot = _url.split('.');
-    var _iframe_src;
-    var _params;
+    // Here, we want to analyze the href contents and pair it off with
+    // a suitable embed URL, extracting parameters using regexp
+
+    var _url = _link.href;         // cache href contents
+    var _by_dot = _url.split('.'); // cache url divided by periods
+    var _iframe_src;               // initialise the variable we will eventually return
+    var _params;                   // initialise an array for URL parameters
 
     // rewrite urls to use https
     _url = _url.match(/http:/) ? _url.replace('http:', 'https:') : _url;
@@ -45,121 +59,169 @@
     // chop off trailing slash
     _url = _url.slice(-1) === '/' ? _url.slice(0, -1) : _url;
 
+    // if-elseif is very verbose with this many conditions,
+    // so we'll use a fairly obscure but entirely valid switch-case instead
     switch (true) {
 
+      // we need to force a boolean expression ('!!') to match to switch(true)
       case !!(_url.match(/\.mp4|\.webm/)):
         if ('HTMLVideoElement' in win) {
+          // may as well embed raw video where/when possible
           _len = _by_dot.length;
           if (_by_dot[_len - 1] === 'mp4' || _by_dot[_len - 1] === 'webm') {
             makeVideo(_url, _link);
+            // _iframe_src is not modified, and returned as empty string, checkDocument's for loop continues
           }
         }
         break;
 
+      case !!(_url.match(/\.mp3|\.m4a|\.wav/)):
+        if ('HTMLAudioElement' in win) {
+          // same goes for audio files
+          _len = _by_dot.length;
+          if (_by_dot[_len - 1] === 'mp3' || _by_dot[_len - 1] === 'm4a' || _by_dot[_len - 1] === 'wav') {
+            makeAudio(_url, _link);
+          }
+        }
+        break;
+
+      // youtube embed url, return unmodified
+      // NB: this catches any embed URL, like playlists, not only videos
       case !!(_url.match(/youtube\.com\/embed\/\w+/)):
         _iframe_src = _url;
         break;
 
+      // youtube standard url, return conversion to embed url
       case !!(_url.match(/youtube\.com\/watch\?v=\w+/)):
         _iframe_src = 'https://www.youtube.com/embed/' + getParams(_url).v;
         break;
 
+      // youtube social share url, return conversion to embed url
       case !!(_url.match(/youtu\.be\/\w+/)):
         _iframe_src = 'https://www.youtube.com/embed/' + _url.match(/[^\/]+$/)[0];
         break;
 
+      // youtube weird /v/ url, return conversion to embed url
       case !!(_url.match(/youtube\.com\/v\/\w+/)):
         _iframe_src = 'https://www.youtube.com/embed/' + _url.match(/[^\/]+$/)[0];
         break;
 
+      // youtube playlist url, return conversion to embed url
       case !!(_url.match(/youtube\.com\/playlist\?list=\w+/)):
         _iframe_src = 'https://www.youtube.com/embed/videoseries?list=' + getParams(_url).list;
         break;
 
+      // vimeo embed url, return unmodified
       case !!(_url.match(/player\.vimeo\.com\/video\/\w+/)):
         _iframe_src = _url;
         break;
 
+      // vimeo standard url, return embed url
       case !!(_url.match(/vimeo\.com\/\w+/)):
         _iframe_src = 'https://player.vimeo.com/video/' + _url.match(/[^\/]+$/)[0] + '?portrait=0';
         break;
 
+      // facebook video url, return embed api url and encode original url
       case !!(_url.match(/facebook\.com\/\w+\/videos\/\w+/)):
         _iframe_src = 'https://www.facebook.com/v2.8/plugins/video.php?href=' + win.encodeURIComponent(_url);
         break;
 
+      // twitch tv embed url, return unmodified (catches channel or video)
+      case !!(_url.match(/player\.twitch\.tv\/\?\w+/)):
+        iframe_src = _url;
+        break;
+
+      // twitch tv clip url, return unmodified
+      case !!(_url.match(/clips\.twitch\.tv\/embed\?clip/)):
+        _iframe_src = _url;
+        break;
+
+      // twitch tv video url, return embed url
       case !!(_url.match(/twitch\.tv\/\w+\/v\/\w+/)):
         _iframe_src = 'https://player.twitch.tv/?video=v' + _url.match(/[^\/]+$/)[0];
         break;
 
-      case !!(_url.match(/twitch\.tv\/\w+/)):
-        _iframe_src = 'https://player.twitch.tv/?channel=' + _url.match(/[^\/]+$/)[0];
-        break;
-
+      // dailymotion embed url, return unmodified
       case !!(_url.match(/dailymotion\.com\/embed\/video\/\w+/)):
         _iframe_src = _url;
         break;
 
+      // dailymotion standard url, return embed url
       case !!(_url.match(/dailymotion\.com\/video\/\w+/)):
         _iframe_src = 'https://www.dailymotion.com/embed/video/' + _url.match(/[^\/]+$/)[0].split('_')[0];
         break;
 
+      // bandcamp embed url, return unmodified
       case !!(_url.match(/bandcamp\.com\/EmbeddedPlayer\/\w+/)):
         _iframe_src = _url;
         break;
 
+      // spotify embed url, return unmodified
       case !!(_url.match(/open\.spotify\.com\/embed\?\w+/)):
         _iframe_src = _url;
         break;
 
+      // spotify embed shorthand url, return unmodified (spotify 301 redirect)
       case !!(_url.match(/embed\.spotify\.com\/\?\w+/)):
         _iframe_src = _url;
         break;
 
+      // spotify playlist url, return speculative embed url
       case !!(_url.match(/spotify\.com\/user\/\w+\/playlist\/\w+/)):
         _iframe_src = 'https://embed.spotify.com/?uri=spotify:user:' + (_url.split('/')[0] !== 'https:' ? _url.split('/')[2] : _url.split('/')[4]) + '/playlist/' + _url.match(/[^\/]+$/)[0];
         break;
 
+      // spotify track url, return embed url
       case !!(_url.match(/spotify\.com\/track\/\w+/)):
         _iframe_src = 'https://embed.spotify.com/?uri=spotify:track:' + _url.match(/[^\/]+$/)[0];
         break;
 
+      // vine standard url, return embed url (NB: vine is shutting down, may not need this)
       case !!(_url.match(/vine\.co\/v\/\w+/)):
         _iframe_src = _url + '/embed/postcard';
         break;
 
+      // soundcloud share url, return embed url (not very useful, but it's there)
       case !!(_url.match(/api\.soundcloud\.com\/tracks\/\w+/)):
         _iframe_src = 'https://w.soundcloud.com/player/?url=' + win.encodeURIComponent(_url) + '&auto_play=false&show_artwork=true&color=0066cc';
         break;
 
+      // imgur album/gallery embed url, return unmodified
       case !!(_url.match(/imgur\.com\/\/a\/\w+\/embed\?/)):
         _iframe_src = _url;
         break;
 
+      // imgur generic image embed url, return unmodified
       case !!(_url.match(/imgur\.com\/\w+\/embed\?/)):
         _iframe_src = _url;
         break;
 
+      // imgur gallery url, return embed url
       case !!(_url.match(/imgur\.com\/gallery\/\w+/)):
         _iframe_src = 'https://imgur.com/a/' + _url.match(/[^\/]+$/)[0] + '/embed?pub=true&analytics=false';
         break;
 
+      // imgur album url, return embed url
       case !!(_url.match(/imgur\.com\/a\/\w+/)):
         _iframe_src = _url + '/embed?pub=true&analytics=false';
         break;
 
+      // gfycat embed url, return unmodified
       case !!(_url.match(/gfycat\.com\/ifr\/\w+/)):
         _iframe_src = _url;
         break;
 
+      // gfycat standard url, return embed url (we have to pattern match here, potentially unreliable)
       case !!(_url.match(/gfycat\.com\/([A-Z][a-z]*)([A-Z][a-z]*)([A-Z][a-z]*)/)):
         _iframe_src = 'https://gfycat.com/ifr/' + _url.match(/[^\/]+$/)[0];
         break;
 
+      // google maps url, pass to static map factory
       case !!(_url.match(/google\..*@[^A-Za-z]+,/)):
         makeStaticMap(_url, _link);
         break;
 
+      // vk search results link with video selected, pass to embedVK function
       case !!(_url.match(/vk\.com\/video\?\w+/)):
         if ('fetch' in win || 'XMLHttpRequest' in win || 'XDomainRequest' in win) {
           _params = getParams(a).z[0].match(/[^video]+$/)[0].split('_');
@@ -169,6 +231,7 @@
         }
         break;
 
+      // vk direct video link, pass to embedVK function
       case !!(_url.match(/vk\.com\/video-\w+/)):
         if ('fetch' in win || 'XMLHttpRequest' in win || 'XDomainRequest' in win) {
           _params = _url.match(/[^\/]+$/)[0].match(/[^video]+$/)[0].split('_');
@@ -178,10 +241,12 @@
         }
         break;
 
+      // if no case matched, or if the only matches needed to be passed along,
+      // return empty string and continue loop
       default:
         _iframe_src = '';
     }
-
+    // returning either empty string or an embeddable URL
     return _iframe_src;
   }
 
@@ -215,31 +280,36 @@
 
 
   function embedVK (_params, _link) {
+    // VK is a special case, as its embed URLs require a hash to be computed from their videos' webpages
+    // the hash is computable with regular expression match, but we need to get the html for the webpage
+    // before we embed it. to do that, we need to use a CORS proxy to ajax the page, compute the hash,
+    // and include the hash as a parameter in the embed URL.
     var _proxy = 'https://cors-anywhere.herokuapp.com/';
     var _vk_url = _proxy + 'https://vk.com/video' + _params[0] + '_' + _params[1];
-    var _xhr = new XMLHttpRequest();
+    var _xhr = 'XMLHttpRequest' in win ? new XMLHttpRequest() : {};
     var _xdr;
 
     if ('fetch' in win) {
-
-      win.fetch(_vk_url, {
-        'mode': 'cors'
-      }).then(function (resp) {
+      // because why not?
+      win.fetch(_vk_url, {'mode': 'cors'}).then(function (resp) {
         return resp.text().then(function (resptxt) {
+
           var _hash = resptxt.match(/hash2[^0-9a-f]*([0-9a-f]*)/)[1];
           var _vk_embed = 'https://vk.com/video_ext.php?oid=' + _params[0] + '&id=' + _params[1] + '&hash=' + _hash  + '&hd=1';
           return makeInlineFrame(_vk_embed, _link, false);
+
         }).catch(function (e) {
           _link.href = _vk_url;
           _link.insertAdjacentHTML('beforeEnd', '<span> [Attempt to embed failed]</span>');
         });
+
       }).catch(function (e) {
         _link.href = _vk_url;
         _link.insertAdjacentHTML('beforeEnd', '<span> [Attempt to embed failed]</span>');
       });
 
     } else if ('XMLHttpRequest' in win && 'withCredentials' in _xhr) {
-
+      // obviously most browsers will support this method
       _xhr.open('GET', _vk_url, true);
       _xhr.responseType = 'text';
 
@@ -258,7 +328,10 @@
 
       _xhr.send(null);
 
-    } else {
+    } else if ('XDomainRequest' in win) {
+      // IE8, IE9 support
+      // NB: absolute no clue whether XDR would actually work for this
+      // or whether string.match works in IE8-9
 
       _xdr = new XDomainRequest();
       _xdr.open('GET', _vk_url);
@@ -274,29 +347,11 @@
         _link.insertAdjacentHTML('beforeEnd', '<span> [Attempt to embed failed]</span>');
       };
 
+      // wrapping send in setTimeout apparently fixes an IE bug... lol
       win.setTimeout(function () {
         _xdr.send();
       }, 0);
 
-    }
-  }
-
-  function makeVideo (url, to_replace) {
-    var _16x9_div = doc.createElement('div');
-    var _video = doc.createElement('video');
-
-    _16x9_div.setAttribute('style', 'position:relative;padding-bottom:56.2%;overflow:hidden;background-color:#444;');
-    _16x9_div.appendChild(_video);
-    _video.setAttribute('style', 'position:absolute;top:0;left:auto;right:auto;bottom:0;margin:0 auto;width:auto;height:100%;overflow:hidden;');
-    _video.setAttribute('controls', 'controls');
-    _video.setAttribute('muted', 'muted');
-    _video.setAttribute('crossorigin', 'anonymous');
-    _video.setAttribute('webkitplaysinline', 'webkitplaysinline');
-    _video.setAttribute('playsinline', 'playsinline');
-    _video.setAttribute('src', url);
-
-    if (to_replace && to_replace.nodeType === 1) {
-      to_replace.parentNode.replaceChild(_16x9_div, to_replace);
     }
   }
 
@@ -326,9 +381,49 @@
     }
   }
 
+  function makeVideo (url, to_replace) {
+    // for the odd occasion where someone posts a link to a raw video
+    var _16x9_div = doc.createElement('div');
+    var _video = doc.createElement('video');
+
+    _16x9_div.setAttribute('style', 'position:relative;padding-bottom:56.2%;overflow:hidden;background-color:#444;');
+    _16x9_div.appendChild(_video);
+    _video.setAttribute('style', 'position:absolute;top:0;left:auto;right:auto;bottom:0;margin:0 auto;width:auto;height:100%;overflow:hidden;');
+    _video.setAttribute('controls', 'controls');
+    _video.setAttribute('muted', 'muted');
+    _video.setAttribute('crossorigin', 'anonymous');
+    _video.setAttribute('webkitplaysinline', 'webkitplaysinline');
+    _video.setAttribute('playsinline', 'playsinline');
+    _video.setAttribute('src', url);
+
+    if (to_replace && to_replace.nodeType === 1) {
+      to_replace.parentNode.replaceChild(_16x9_div, to_replace);
+    }
+  }
+
+  function makeAudio(url, to_replace) {
+    // for the even rarer occasion that someone should post raw audio
+    var _4x1_div = doc.createElement('div');
+    var _audio = doc.createElement('audio');
+
+    _4x1_div.setAttribute('style', 'position:relative;padding-bottom:22%;overflow:hidden;background-color:#444;');
+    _4x1_div.appendChild(_audio);
+    _audio.setAttribute('style', 'position:absolute;top:0;left:auto;right:auto;bottom:0;margin:0 auto;width:auto;height:100%;overflow:hidden;');
+    _audio.setAttribute('controls', 'controls');
+    _audio.setAttribute('muted', 'muted');
+    _audio.setAttribute('crossorigin', 'anonymous');
+
+    if (to_replace && to_replace.nodeType === 1) {
+      to_replace.parentNode.replaceChild(_4x1_div, to_replace);
+    }
+  }
+
   function makeStaticMap (url, to_replace) {
+    // direct google maps URLs are not trival to predict/decode, but will typically include latitude and longitude
+    // which, fortuitously, is the only requirement for displaying a suitable static maps API substitute
     var _16x9_div = doc.createElement('div');
     var _img = doc.createElement('img');
+    // latlang is trivial to extract by means of regex, as is the zoom level
     var _latlang = url.match(/@([^A-Za-z]+,)/)[0].slice(0,-1).replace('@', '');
     _16x9_div.setAttribute('style', 'position:relative;padding-bottom:56.2%;overflow:hidden;background-color:#444;cursor:pointer;');
 
@@ -338,11 +433,15 @@
 
     if ('transform' in _img.style) {
       _img.setAttribute('style', 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);min-width:100%;min-height:100%;width:auto;height:auto;');
+    } else if ('webkitTransform' in _img.style) {
+      _img.setAttribute('style', 'position:absolute;top:50%;left:50%;-webkit-transform:translate(-50%,-50%);min-width:100%;min-height:100%;width:auto;height:auto;');
     } else {
       _img.setAttribute('style', 'width:100%;height:auto;');
     }
 
-    _img.alt = 'Google Map';
+    // build the static maps URL. sticking to an aspect ratio
+    // enables us to use clientWidth/Height to get an appropriate size
+    // for the static map image
     _img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
                _latlang +
                '&size=' + _16x9_div.clientWidth + 'x' + _16x9_div.clientHeight +
@@ -351,29 +450,39 @@
                '&markers=' + _latlang +
                '&key=AIzaSyAf7V-aqUb-Guull54mvfrH61hFUbNPqvM';
 
+    _img.alt = 'Google Map';
     _16x9_div.appendChild(_img);
-    _16x9_div.addEventListener('click', function(){
+
+    // it's a static map, so it should be possible to provide a clickable link to an interactive map
+    // if we create a standard anchor element, it will be grabbed by getElementsByTagName
+    // we can safely say javascript is enabled, so attach an event listener to the 16:9 div instead
+    _16x9_div.addEventListener('click', function() {
       win.open(url);
     }, false);
 
   }
 
   function isInViewport (el) {
+    // simple but effective and well supported
     var r = el.getBoundingClientRect();
     return (r.top >= 0 && r.left >= 0 && r.top <= (win.innerHeight || doc.documentElement.clientHeight));
   }
 
-  function debounce (f, wait) {
+  function debounce (func, wait) {
+    // a debouncing function, for the expensive scroll event
     var scheduled, args, context, timestamp;
     return function () {
       context = this;
       args = [];
       timestamp = win.performance.now;
 
-      for (var i = 0; i < arguments.length; ++i){
+      // NB: arguments is never accessed directly. it is aliased to a new array
+      // and passed with func.apply. this means v8 won't choke.
+      for (var i = 0; i < arguments.length; ++i) {
         args[i] = arguments[i];
       }
 
+      // the timeout is also recycled as opposed to being recreated each time.
       if (!scheduled) {
        scheduled = win.setTimeout(later, wait);
       }
@@ -384,7 +493,7 @@
           scheduled = win.setTimeout(later, wait - last);
         } else {
           scheduled = null;
-          f.apply(context, args);
+          func.apply(context, args);
         }
       }
     };
