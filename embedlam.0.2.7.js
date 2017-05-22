1514fcd217f2101ffb82b4ad923f12b9d5716f72
A. M. Douglas <me@amdouglas.com>
2017-05-23 00:16:53 +0100

    Update embedlam.0.2.7.js

--- embedlam.0.2.7.js
+++ embedlam.0.2.7.js
@@ -1,4 +1,4 @@
-/* embedlam.js @ 0.2.3 :: BSD-3-Clause-Clear :: https://github.com/wnda/embedlam/ */
+/* embedlam.js @ 0.2.7 :: BSD-3-Clause-Clear :: https://github.com/wnda/embedlam/ */
 ;(function (win, doc) {
 
   'use strict';
