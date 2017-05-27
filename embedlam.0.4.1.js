cd157edc913860e6484a64711d64146526b96d64
A. M. Douglas <me@amdouglas.com>
2017-05-27 17:40:51 +0100

    Update and rename embedlam.0.4.0.js to embedlam.0.4.1.js

--- embedlam.0.4.1.js
+++ embedlam.0.4.1.js
@@ -1,4 +1,4 @@
-/* embedlam.js @ 0.4.0 :: BSD-3-Clause-Clear :: https://github.com/wnda/embedlam/ */
+/* embedlam.js @ 0.4.1 :: BSD-3-Clause-Clear :: https://github.com/wnda/embedlam/ */
 ;(function (win, doc) {
 
   'use strict';
